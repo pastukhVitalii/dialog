b037b10705b8d88d14e05a019e7ed3db1ef2c12f
pastukhVitalii <pastukh.v.7@gmail.com>
2020-04-02 20:59:12 +0300

    Add components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Messages from "./component/Messages/Messages";
+
+function App() {
+  return (
+    <div className="App">
+      <Messages/>
+    </div>
+  );
+}
+
+export default App;
